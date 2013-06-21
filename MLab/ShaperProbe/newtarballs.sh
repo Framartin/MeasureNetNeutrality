3c612c97ce1850be998efcf987e7b1c2332ad2a3
Framartin <framartin@framasoft.org>
2013-06-21 17:12:56 +0200

    Changing sort -u to make difference between files

--- MLab/ShaperProbe/newtarballs.sh
+++ MLab/ShaperProbe/newtarballs.sh
@@ -1,5 +1,10 @@
+################################################################
+#                           Main script                        #
+################################################################
 #
-#
+# This script update the list of shaperprobe's tarballs, and execute traitement.sh for those which are not already done.
+# To limit the size of tarballs on your hard drive, the script download, treat and remove each tarballs (marked as not done) one by one.
+# You can execute this script regulary thanks to cron, for example once a day or once a week.
 #
 cd . # placer le bon répertoire
 date >> ./errors/update_gsutil.txt
@@ -7,17 +12,23 @@ gsutil update >& ./errors/update_gsutil.txt
 date >> ./errors/latest_tarballs.txt
 date >> ./errors/download_tarballs.txt
 gsutil ls -R gs://m-lab/shaperprobe/** > latest_tarballs.txt 2>> ./errors/latest_tarballs.txt      # download the list of all tarballs
-cat latest_tarballs.txt done_tarballs.txt > ./tmp/compare.txt
-sort -u ./tmp/compare.txt       # keep only tarballs which are not already done !!!!!! BE CAREFUL SEEMS NOT WORKING
+rm ./tmp/tarballs_to_do.txt
+for ligne in $(cat latest_tarballs.txt)      # put into tarballs_to_do.txt every tarballs of latest_tarballs.txt which are not in done_tarballs.txt
+do
+    if (! grep $ligne done_tarballs.txt) ; then
+        echo $ligne >> ./tmp/tarballs_to_do.txt
+    fi
+done
 old_IFS=$IFS     # sauvegarde du séparateur de champ  
 IFS=$'\n'        # nouveau séparateur de champ, le caractère fin de ligne
-for ligne in $(cat ./tmp/compare.txt)      # download and treat new tarballs one by one
+for ligne in $(cat ./tmp/tarballs_to_do.txt)      # download and treat new tarballs one by one
 do
     echo $ligne | gsutil cp -I ./tmp/tarballs/ 2>> ./errors/download_tarballs.txt && echo $ligne >> ./tmp/downloaded_tarballs.txt && ./traitement.sh $ligne && echo $ligne >> done_tarballs.txt
 done
 IFS=$old_IFS
 
 # Differences between ./tmp/downloaded_tarballs.txt and done_tarballs.txt are tarballs which failed the treatment
+# Execute : diff ./tmp/downloaded_tarballs.txt done_tarballs.txt
 
 # If folder /tmp/tarballs is not empty after treatment, it's not normal
 
