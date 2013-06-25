381b0f447b48531ebdf51b4645cd1fe7d1025362
Framartin <framartin@framasoft.org>
2013-06-25 19:03:35 +0200

    Add extraction of up median recieved rate

--- MLab/ShaperProbe/traitement.sh
+++ MLab/ShaperProbe/traitement.sh
@@ -27,8 +27,16 @@ do
             elif UPSTREAMLINE=$(grep "Upstream:" $TXTFILEWE.clean) ; then
                 if grep "No shaper detected" $UPSTREAMLINE ; then
                     UPSHAPER="\"no\" \"no\" \"no\""
+                    LINENUMBER=$(grep -n "Upstream:" $TXTFILEWE.clean | cut -d: -f1)   # if no shaper is detected, the following line CAN contain the median received rate
+                    LINENUMBER=$((LINENUMBER+1))
+                    awk "NR==$LINENUMBER" $TXTFILEWE.clean > upmedianrateline.tmp      # extract this line
+                    if grep "Median received rate" upmedianrateline.tmp ; then         # test if this line contain the rate
+                        UPMEDIANRATE=$(cat upmedianrateline.tmp | sed -n -e 's/^Median received rate. \([0-9]*\) [KkBbPpSs]*.*/\1/p')   #extract the median recieved rate
+                    fi
+                    rm upmedianrateline.tmp
                 elif grep "Burst size:" $UPSTREAMLINE ; then
                     UPSHAPER=$(echo $UPSTREAMLINE | sed -n -e 's/^Upstream: Burst size: \([0-9]*\)-\([0-9]*\) [Kk][Bb]; Shaping rate: \([0-9]*\) [KkBbPpSs]*.*/\1 \2 \3/p')
+                    UPMEDIANRATE="\"no\""    # if a shaper is detected, the median received rate after the shape, is the shaping rate 
                 else
                     echo "The syntax of this log is not supported (no Up Burst)"
                     echo "Syntax of $f of tarball $TARFILE not supported (no Up Burst)" >> ../../errors/non_supported_syntax.txt
@@ -40,6 +48,7 @@ do
                     DOWNSHAPER="\"no\" \"no\" \"no\""
                 elif grep "Burst size:" $DOWNSTREAMLINE ; then
                     DOWNSHAPER=$(echo $DOWNSTREAMLINE | sed -n -e 's/^Downstream: Burst size: \([0-9]*\)-\([0-9]*\) [Kk][Bb]; Shaping rate: \([0-9]*\) [KkBbPpSs]*.*/\1 \2 \3/p')
+                    DOWNMEDIANRATE="\"no\""
                 # BUG : Two types if log. Some with a interval and some with only one value.
                 # Downstream: Burst size: 3696-4886 KB; Shaping rate: 5077 Kbps.
                 # Downstream: Burst size: 11755 KB; Shaping rate: 6272 Kbps.
@@ -59,11 +68,11 @@ do
                 fi
                 IPDATE=$(echo $TXTFILE | sed -n -e 's/^\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\)_\([0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)T\([0-9]\{2\}\):\([0-9]\{2\}\).*/"\1" \2 \3 \4 \5 \6/p')    # file names seems to be constructed similary whatever client version. This line extract the IP the date and the time from the file name
                 SERVER=$(echo $TARFILE | sed -n -e 's/^[0-9a-zA-Z]*Z-\([0-9a-zA-Z]*-[0-9a-zA-Z]*\)-shaperprobe.*/\1/p') # Extract server name from the name of the tarball
-                head -n 3 $TXTFILEWE.clean > headlog.txt       # save head of the file
-                if SLEEPTIMELINE=$(grep "sleep time resolution" headlog.txt) ; then          # extract the sleep time resolution (in ms)
+                head -n 3 $TXTFILEWE.clean > headlog.tmp       # save head of the file
+                if SLEEPTIMELINE=$(grep "sleep time resolution" headlog.tmp) ; then          # extract the sleep time resolution (in ms)
                     SLEEPTIME=$(echo $SLEEPTIMELINE | sed -n -e 's/^sleep time resolution. \([0-9]*\.[0-9]*\) [MmSs]*.*/\1/p')
                 fi
-                if VERSIONLINE=$(grep "Client version" headlog.txt) ; then                   # extract the client version
+                if VERSIONLINE=$(grep "Client version" headlog.tmp) ; then                   # extract the client version
                     VERSION=$(echo $VERSIONLINE | sed -n -e 's/^Client version. \([0-9]*\).*/\1/p')
                 fi
                 echo $IPDATE \"$SERVER\" $VERSION $SLEEPTIME $UPSHAPER $DOWNSHAPER $UPMEDIANRATE $DOWNMEDIANRATE $UPCAPACITY $DOWNCAPACITY >> ../../csv/$TARFILEWE.csv # attention mettre des virgules en séparation.
@@ -81,7 +90,7 @@ do
     fi
 done
 rm $TARFILE
-rm headlog.txt
+rm headlog.tmp
 cd ../..
 
 
