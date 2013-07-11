da4abd1e5972163b48a89e7651d197086a270ab5
Hexalyse <reversebob@gmail.com>
2013-07-11 20:25:30 +0200

    Renamed treatment.sh to process_tarball.sh because treatment is not correct here. Corrected typos and english errors in comments.

--- MLab/ShaperProbe/main.sh
+++ MLab/ShaperProbe/main.sh
@@ -3,13 +3,15 @@
 ################################################################
 #
 #---------------------------------------------------------------------------------------------------------#
-# PLEASE MAKE SURE THAT YOU HAVE ALREADY EXECUTE initialization.sh ONE (and only one) TIME BEFORE main.sh #
+# PLEASE MAKE SURE THAT YOU HAVE ALREADY EXECUTED initialization.sh ONCE (and only once) BEFORE main.sh   #
 #---------------------------------------------------------------------------------------------------------#
 #
-# Execute ONLY one main.sh at the same time !
+# Execute ONLY one instance of main.sh at the same time !
 #
-# This script update the list of shaperprobe's tarballs, and execute treatment.sh for those which are not already done.
-# To limit the size of tarballs on your hard drive, the script download, treat and remove each tarballs (marked as not done) one by one.
+# This script updates the list of shaperprobe's tarballs, and executes process_tarball.sh for those which are not already done.
+# To limit the size of tarballs on your hard drive, the script downloads, process and removes each tarballs (marked as not done) one by one.
+# UPDATE : two tarballs are now stocked on your hard drive at a given moment to optimise speed (download of the next tarball is
+#   executed in parallel of the processing of the current tarball)
 # You can execute this script regulary thanks to cron, for example once a day or once a week.
 #
 date >> ./errors/update_gsutil.txt
@@ -26,24 +28,24 @@ sort -o done_tarballs.tmp done_tarballs.txt
 comm -3 latest_tarballs.tmp done_tarballs.tmp > ./tmp/tarballs_to_do.txt # delete line that are in both files
 rm -f latest_tarballs.tmp
 rm -f done_tarballs.tmp
-# download the next tgz and treat the current tarball at the same time
+# download the next tgz and process the current tarball at the same time
 ligne_old=$(head -n 1 ./tmp/tarballs_to_do.txt)
 echo $ligne_old | gsutil cp -I ./tmp/tarballs/ 2>> ./errors/download_tarballs.txt && echo $ligne_old >> ./tmp/downloaded_tarballs.txt # download the first tgz
-for ligne in $(tail -n +2 ./tmp/tarballs_to_do.txt)      # download and treat new tarballs one by one
+for ligne in $(tail -n +2 ./tmp/tarballs_to_do.txt)      # download and process new tarballs one by one
 do
     echo $ligne | gsutil cp -I ./tmp/tarballs/ 2>> ./errors/download_tarballs.txt && echo $ligne >> ./tmp/downloaded_tarballs.txt &   # download the next tarball
     PID1=$!
-    ./treatment.sh $ligne_old && echo $ligne_old >> done_tarballs.txt
+    ./process_tarball.sh $ligne_old && echo $ligne_old >> done_tarballs.txt
     wait $PID1
     ligne_old=$ligne
 done
-./treatment.sh $ligne_old && echo $ligne_old >> done_tarballs.txt  # treat the last tarball
+./process_tarball.sh $ligne_old && echo $ligne_old >> done_tarballs.txt  # process the last tarball
 IFS=$old_IFS
 rm -rf ./tmp/tarballs/files/*
 
-# Differences between ./tmp/downloaded_tarballs.txt and done_tarballs.txt are tarballs which failed the treatment
+# Differences between ./tmp/downloaded_tarballs.txt and done_tarballs.txt are tarballs which failed the process
 # Execute : diff ./tmp/downloaded_tarballs.txt done_tarballs.txt
-# The folder /tmp/tarballs should be empty after treatment (exept the empty sub-folder called "files")
+# The folder /tmp/tarballs should be empty after processing (except the empty sub-folder called "files")
 
 echo "IP,year,month,day,hour,minute,server,clientversion,sleeptime,minupburstsize,maxupburstsize,upshapingrate,mindownburstsize,maxdownburstsize,downshapingrate,upmedianrate,downmedianrate,upcapacity,downcapacity" > data.csv    # head of the csv file
 cat ./csv/*.csv >> data.csv
