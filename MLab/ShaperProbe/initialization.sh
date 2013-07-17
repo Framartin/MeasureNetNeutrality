275c6c2e4e2188409048552112f51b94af0e86c7
Martin G <framartin@framasoft.org>
2013-07-17 15:12:35 +0200

    Place new csv files in csv/new

--- MLab/ShaperProbe/initialization.sh
+++ MLab/ShaperProbe/initialization.sh
@@ -10,11 +10,13 @@ mkdir tmp
 mkdir tmp/tarballs
 mkdir tmp/tarballs/files
 mkdir csv
+mkdir csv/new
 touch done_tarballs.txt
 echo "The following log files are not processed because they are not standards, but their tarballs are marked as done. This is generally normal that some appear here, because some tests are aborted (then some logs are incomplete)." > errors/non_standard_logs_no_downstream.txt
 echo "The following log files are not processed because they are not standards, but their tarballs are marked as done. This is generally normal that some appear here, because some tests are aborted (then some logs are incomplete)." > errors/non_standard_logs_no_upstream.txt
 chmod +x process_tarball.sh
 chmod +x main.sh
+chmod +x check_csv.sh
 mv initialization.sh initialization.sh.done    # the script is marked as done
 #
 ####################################################################
