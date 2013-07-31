112abe423b1cc12667f7cd36ef2a9b9f722f6809
Martin G <framartin@framasoft.org>
2013-07-31 12:52:56 +0200

    First draft of the mysql's tables

--- MLab/ShaperProbe/initialization.sh
+++ MLab/ShaperProbe/initialization.sh
@@ -40,6 +40,104 @@ MYSQL_PASSWD=$(sed -n -e 's/^MYSQL_PASSWORD="\([^"]*\)"$/\1/p' mysql.conf)
 MYSQL_DB=$(sed -n -e 's/^MYSQL_DB="\([^"]*\)"$/\1/p' mysql.conf)
 # create tables
 mysql -u "${MYSQL_USER}" -p"${MYSQL_PASSWD}" -h localhost -D ${MYSQL_DB} <<EOF
-    
+CREATE TABLE Shaperprobe (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    ip VARCHAR(15) NOT NULL,
+    date_test DATETIME NOT NULL,
+    server VARCHAR(30),
+    client_version TINYINT UNSIGNED,
+    sleeptime DECIMAL(4,2),
+    minupburstsize VARCHAR(8), -- need to be changed to INT
+    maxupburstsize VARCHAR(8), -- need to be changed to INT
+    upshapingrate VARCHAR(8), -- need to be changed to INT
+    mindownburstsize VARCHAR(8), -- need to be changed to INT
+    maxdownburstsize VARCHAR(8), -- need to be changed to INT
+    downshapingrate VARCHAR(8), -- need to be changed to INT
+    upmedianrate VARCHAR(8), -- need to be changed to INT
+    downmedianrate VARCHAR(8), -- need to be changed to INT
+    upcapacity DECIMAL(10,2),
+    downcapacity DECIMAL(10,2),
+    upshape VARCHAR(5), -- attention vérifier qu'il est bien dans le csv ; 'TRUE' or 'FALSE'
+    downshape VARCHAR(5), -- attention vérifier qu'il est bien dans le csv ; 'TRUE' or 'FALSE'
+    data_quality TINYINT UNSIGNED, -- attention vérifier qu'il est bien dans le csv ;
+    PRIMARY KEY (id)
+)
+ENGINE=INNODB;
+CREATE TABLE Shaperprobe_TMP (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    ip VARCHAR(15) NOT NULL,
+    date_test DATETIME NOT NULL,
+    server VARCHAR(30),
+    client_version TINYINT UNSIGNED,
+    sleeptime DECIMAL(4,2),
+    minupburstsize VARCHAR(8), -- need to be changed to INT
+    maxupburstsize VARCHAR(8), -- need to be changed to INT
+    upshapingrate VARCHAR(8), -- need to be changed to INT
+    mindownburstsize VARCHAR(8), -- need to be changed to INT
+    maxdownburstsize VARCHAR(8), -- need to be changed to INT
+    downshapingrate VARCHAR(8), -- need to be changed to INT
+    upmedianrate VARCHAR(8), -- need to be changed to INT
+    downmedianrate VARCHAR(8), -- need to be changed to INT
+    upcapacity DECIMAL(10,2),
+    downcapacity DECIMAL(10,2),
+    upshape VARCHAR(5), -- attention vérifier qu'il est bien dans le csv ; 'TRUE' or 'FALSE'
+    downshape VARCHAR(5), -- attention vérifier qu'il est bien dans le csv ; 'TRUE' or 'FALSE'
+    data_quality TINYINT UNSIGNED, -- attention vérifier qu'il est bien dans le csv ;
+    PRIMARY KEY (id)
+)
+ENGINE=INNODB;
+CREATE TABLE Localisation_IP (
+    ip VARCHAR(15) NOT NULL,
+    country_code VARCHAR(2),
+    country_name VARCHAR(50),
+    loc_id MEDIUMINT UNSIGNED NOT NULL, -- city code
+    city_name VARCHAR(255),
+    region_code VARCHAR(2),
+    region_name VARCHAR(50),
+    PRIMARY KEY (ip)
+)
+ENGINE=INNODB;
+CREATE TABLE Geolite_country (
+    begin_ip VARCHAR(15),
+    end_ip VARCHAR(15),
+    begin_ip_num INT UNSIGNED,
+    end_ip_num INT UNSIGNED,
+    country_code VARCHAR(2),
+    country_name VARCHAR(50)
+)
+ENGINE=INNODB;
+CREATE TABLE Geolite_region_code (
+    country_code VARCHAR(2),
+    region_code VARCHAR(2),
+    region_name VARCHAR(50)
+)
+ENGINE=INNODB;
+CREATE TABLE Geolite_city_blocks (
+    begin_ip_num INT UNSIGNED NOT NULL,
+    end_ip_num INT UNSIGNED NOT NULL,
+    loc_id MEDIUMINT UNSIGNED NOT NULL,
+)
+ENGINE=INNODB;
+CREATE TABLE Geolite_city_location (
+    loc_id MEDIUMINT UNSIGNED NOT NULL,
+    country_code VARCHAR(2),
+    region_code CHAR(2),
+    city_name VARCHAR(255),
+    postal_code VARCHAR(6),
+    latitude DECIMAL(7,4),
+    longitude DECIMAL(7,4),
+    metro_code SMALLINT UNSIGNED,
+    area_code char(3),
+    PRIMARY KEY (loc_id)
+)
+ENGINE=INNODB;
+-- CREATE TABLE As_name (
+--    ip VARCHAR(15) NOT NULL,
+--    asn,
+--    allocated,
+--    country_code,
+-- )
+-- ENGINE=INNODB;
 EOF
 mv initialization.sh initialization.sh.done    # the script is marked as done
+
