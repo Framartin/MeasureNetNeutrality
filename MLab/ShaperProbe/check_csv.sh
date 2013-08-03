349431d1f6a2d69e8da7223aed500f2d1d1a75b9
Framartin <framartin@framasoft.org>
2013-08-03 20:00:05 +0200

    Update integrity check

--- MLab/ShaperProbe/check_csv.sh
+++ MLab/ShaperProbe/check_csv.sh
@@ -3,7 +3,7 @@ cd csv/new
 IFS=$'\n'
 for CSVFILE in *.csv
 do
-     grep -E "^\"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\",[0-9]{4},[0-9]{2},[0-9]{2},[0-9]{2},[0-9]{2},\"[a-zA-Z0-9\-]+\",(NA|[0-9]+),(NA|[0-9]*\.[0-9]*),(NA|\"no\"|[0-9]+),(NA|\"no\"|[0-9]+),(NA|\"no\"|[0-9]+),(NA|\"no\"|[0-9]+),(NA|\"no\"|[0-9]+),(NA|\"no\"|[0-9]+),(NA|\"no\"|[0-9]+),(NA|\"no\"|[0-9]+),(NA|[0-9]+\.[0-9]+),(NA|[0-9]+\.[0-9]*)$" $CSVFILE > ../clean/$CSVFILE
+     grep -E "^NULL,\"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\",\"[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\",\"[a-zA-Z0-9\-]+\",(NULL|[0-9]+),(NULL|[0-9]*\.[0-9]*),(\"TRUE\"|\"FALSE\"|NULL),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+),(\"TRUE\"|\"FALSE\"|NULL),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+\.[0-9]+),(NULL|[0-9]+\.[0-9]*),NULL$" $CSVFILE > ../clean/$CSVFILE
      sort -o $CSVFILE.tmp $CSVFILE
      sort -o ../clean/$CSVFILE.tmp ../clean/$CSVFILE
      comm -3 $CSVFILE.tmp ../clean/$CSVFILE.tmp > ../cleaning_errors/$CSVFILE # keep the difference between the two files : lines where errors occurs
