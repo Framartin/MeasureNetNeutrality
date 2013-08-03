35e1c5e6da4d2d4f0bad6fd1025a6bba7b3b1e69
Martin G <framartin@framasoft.org>
2013-08-03 10:01:48 +0200

    Add connection to mysql

--- MLab/ShaperProbe/main.sh
+++ MLab/ShaperProbe/main.sh
@@ -57,6 +57,11 @@ echo "IP,year,month,day,hour,minute,server,clientversion,sleeptime,minupburstsiz
 
 # POST-TREATEMENT
 
+# set variables to be able to connect to mysql
+MYSQL_USER=$(sed -n -e 's/^MYSQL_USER="\([^"]*\)"$/\1/p' mysql.conf)
+MYSQL_PASSWD=$(sed -n -e 's/^MYSQL_PASSWORD="\([^"]*\)"$/\1/p' mysql.conf)
+MYSQL_DB=$(sed -n -e 's/^MYSQL_DB="\([^"]*\)"$/\1/p' mysql.conf)
+
 # download Geolite databases (IP Geolocation)
 # For more information about Geolite, please visit, http://dev.maxmind.com/geoip/legacy/geolite/
 cd databases
@@ -64,7 +69,10 @@ wget -N -q "http://geolite.maxmind.com/download/geoip/database/GeoIPCountryCSV.z
 if [ -e GeoIPCountryCSV.zip.1 ] ; then  # true if a new version was downloaded
     unzip -o GeoIPCountryCSV.zip
     mv GeoIPCountryCSV.zip.1 GeoIPCountryCSV.zip
-    # delete all the lines on the mysql table and import the new csv
+    mysql -u "${MYSQL_USER}" -p"${MYSQL_PASSWD}" -h localhost -D ${MYSQL_DB} <<EOF
+DELETE FROM Geolite_country;
+-- import lines
+EOF
 fi
 
 # Team Cymru's Whois (AS Name database)
