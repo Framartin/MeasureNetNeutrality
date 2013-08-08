374e443e755c05ef066ac0d6e58e9dd9f34a45be
Framartin <framartin@framasoft.org>
2013-08-08 16:50:25 +0200

    Update check_csv.sh

--- MLab/ShaperProbe/check_csv.sh
+++ MLab/ShaperProbe/check_csv.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# check all lines of csv files in the folder csv/new/raw and copy correct lines in csv/new/clean and not correct ones in csv/new/cleaning_errors
 cd csv/new/raw
 IFS=$'\n'
 for CSVFILE in *.csv
 do
-     grep -E "^NULL,\"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\",\"[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\",\"[a-zA-Z0-9\-]+\",(NULL|[0-9]+),(NULL|[0-9]*\.[0-9]*),(\"TRUE\"|\"FALSE\"|NULL),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+),(\"TRUE\"|\"FALSE\"|NULL),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+\.[0-9]+),(NULL|[0-9]+\.[0-9]*),NULL$" $CSVFILE > ../clean/$CSVFILE
+"192.33.210.16","2009-05-05 23:56:20","batch-batch",NULL,NULL,"FALSE",NULL,NULL,NULL,"FALSE",NULL,NULL,NULL,NULL,NULL,7294.83,10091.00
+     grep -E "^\"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\",\"[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\",\"[a-zA-Z0-9\-]+\",(NULL|[0-9]+),(NULL|[0-9]*\.[0-9]*),(\"TRUE\"|\"FALSE\"|NULL),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+),(\"TRUE\"|\"FALSE\"|NULL),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+),(NULL|[0-9]+\.[0-9]+),(NULL|[0-9]+\.[0-9]*)$" $CSVFILE > ../clean/$CSVFILE
      sort -o $CSVFILE.tmp $CSVFILE
      sort -o ../clean/$CSVFILE.tmp ../clean/$CSVFILE
      comm -3 $CSVFILE.tmp ../clean/$CSVFILE.tmp > ../cleaning_errors/$CSVFILE # keep the difference between the two files : lines where errors occurs
